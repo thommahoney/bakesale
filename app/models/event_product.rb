1d7a7621c5a2b0f1e0c941655b50ed394a8ff3b7
Thom Mahoney <mahoneyt@gmail.com>
2015-08-22 13:29:25 -0700

    add event_products

---
+++ app/models/event_product.rb
@@ -0,0 +1,2 @@
+class EventProduct < ActiveRecord::Base
+end
---
+++ test/models/event_product_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EventProductTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
