ada32ab245a753fc5ab5341835816da1be66d6a2
Thom Mahoney <mahoneyt@gmail.com>
2015-08-22 18:31:45 -0700

    add login via digits api

---
+++ app/assets/javascripts/sessions.js
@@ -0,0 +1,39 @@
+$(function() {
+  Digits.init({ consumerKey: 'BHreRvWpQaZQlNuzM9of6SauZ' });
+
+  $('#loginButton').click(function(event) {
+    event.preventDefault();
+
+    Digits.logIn()
+      .done(function(response) {
+        // Send headers to your server and validate user by calling Digits’ API
+        var oAuthHeaders = response.oauth_echo_headers;
+        var verifyData = {
+          authHeader: oAuthHeaders['X-Verify-Credentials-Authorization'],
+          apiUrl: oAuthHeaders['X-Auth-Service-Provider']
+        };
+
+        $.ajax({
+            url: '/sessions',
+            type: 'POST',
+            data: verifyData,
+            success: function(data) {
+              console.log('success', data);
+              if(data.full_name) {
+                // this is a new user
+                window.location = '/users/' + data.id + '/edit';
+              } else {
+                // we know about this user
+                window.location = '/organizations';
+              }
+            },
+            error: function(xhr, textStatus, errorThrown) {
+              console.log('error', xhr, textStatus);
+            }
+        });
+      })
+    .fail(function(error) {
+      console.log('an error occured:', error);
+    });
+  });
+});
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.where(user_params).first
+    # make an http request to digits api to get user information
+    response = Typhoeus.get(params[:apiUrl], headers: { 'Authorization' => params[:authHeader] })
+    # if we get user information back, find a user with the given phone number
+    user_information = JSON.parse(response.body)
+    phone_number = user_information['phone_number']
+    user = User.where(phone: phone_number).first
+    # if we don't know the user, create a new one
+    unless user
+      user = User.create!(phone: phone_number)
+    end
+    # log in the user
     session[:user_id] = user.id
 
-    redirect_to organizations_path
+    render json: user
   end
 
   def logout
